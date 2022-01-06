2f12e9da5f5c3b166b771db5e2a55c6ca196cf7f
Fera <christy.ferani@gmail.com>
2022-01-07 02:06:21 +0700

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,164 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+        <div class="col-md-12">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100 slideshow-img" src="images/home/slideshow_batik_1.jpg" alt="Batik">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100 slideshow-img" src="images/home/slideshow_batik_2.jpg" alt="Batik">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100 slideshow-img" src="images/home/slideshow_batik_3.jpg" alt="Batik">
+                  </div>
                 </div>
-            </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
         </div>
+    </div>
+    <div class="row p-3 mt-3">
+        <div class="col-4 text-center ">
+            <img class="mb-4 w-25 " src="images/icon/quality.png" alt="Quality">
+            <h4> Kualitas Bermutu Tinggi </h4>
+            <p> Kami menyediakan berbagai macam batik dengan kualitas yang terbaik dari tangan pertama </p>
+        </div>
+        <div class="col-4 text-center">
+            <img class="mb-4 w-25 " src="images/icon/price.png" alt="Price">
+            <h4> Harga Terjangkau </h4>
+            <p> Kami memiliki produk dengan harga yang lebih murah dan terjangkau dari toko batik lainnya. </p>
+        </div>
+        <div class="col-4 text-center">
+            <img class="mb-4 w-25 " src="images/icon/courier.png" alt="Courier">
+            <h4> Pengiriman Cepat </h4>
+            <p> Pengiriman akan langsung di proses dan akan tiba di rumah Anda pada h+1 pembayaran. </p>
+        </div>
+    </div>
+
+    {{-- Produk terlaris 4 row aja --}}
+    <div class="row mt-5">
+        <h2 class="font-weight-bold mb-4 sub-title text-center"> Produk Terlaris </h2>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        
+
+        <h2 class="font-weight-bold mb-4 mt-5 sub-title text-center"> Produk Terbaru </h2>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img" src="images/product/product1.jpg" alt="Batik">
+                <div class="card-body">
+                  <h4 class="card-title"> Baju Batik Modern </h4>
+                  <h6 class="card-subtitle mb-2 text-muted">Ukuran: L</h6>
+                  <p class="card-text">
+                    Baju batik modern wanita yang dibuat dengan bahan katun, ketebalan sedang. Cocok untuk dipakai acara formal maupun semi formal </p>
+                </div>
+                <div class="buy d-flex justify-content-between align-items-center px-3 pb-2">
+                    <div class="price text-success"><h5 class="mt-2">Rp 100.000</h5></div>
+                     <a href="#" class="btn btn-danger mt-1"> Beli </a>
+                </div>
+            </div>     
+        </div>
+
+    </div>
+
+    <div class="row mt-5">
+        <a href="" class="text-decoration-none text-muted"><p class="text-center"> Produk Lainnya </p></a>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,49 +7,59 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Crushed&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-black shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand navbar-logo-font" href="{{ url('/') }}" id="logo">
+                    {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
+                        <input type="text" placeholder="Telusuri" class="search-bar"/>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> Shop </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=""> About Batik </a>
+                        </li>
                         @guest
-                            @if (Route::has('login'))
+                            @if(Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
@@ -58,9 +68,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="">
+                                        Account
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -78,6 +91,30 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="footer-dark pt-5 pb-2 text-center">
+            <h1 id="logo" class="text-white"> Batikin.id </h1>
+            <a href="facebook.com" class="text-decoration-none">
+                <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                    <i class="fa fa-facebook-f fa-stack-1x text-dark"></i>
+                </span> 
+            </a>
+            <a href="twitter.com" class="text-decoration-none">
+                <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                    <i class="fa fa-twitter fa-stack-1x text-dark"></i>
+                </span> 
+            </a>
+            <a href="instagram.com" class="text-decoration-none">
+                <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x fa-inverse"></i>
+                <i class="fa fa-instagram fa-stack-1x text-dark"></i>
+                </span>
+            </a>
+            <p class="mt-2 text-muted"> Copyright 2022 &copy; <span id="logo" class="text-white"> Batikin.id </span></p>
+        </footer>
+
     </div>
 </body>
 </html>
